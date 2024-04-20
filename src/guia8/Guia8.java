2b8bfb8d416873d2800c9520bfed389d780558f9
Duvan Camilo Amaya Lopez <duvan.amayalopez@unicolombo.edu.co>
2024-04-20 13:21:42 -0500

    Guia 8

---
+++ src/guia8/Guia8.java
@@ -0,0 +1,12 @@
+package guia8;
+
+import VentanaGuia8.VentanaPrincipal;
+
+public class Guia8 {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setLocationRelativeTo(ventana);
+        ventana.setVisible(true);
+    }
+    
+}
